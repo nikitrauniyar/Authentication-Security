d1a2b146b3e7b99e9cecb5f2d222652263b19bfd
nikitrauniyar <rauniyarnikit@gmail.com>
2020-07-14 19:32:40 +1000

    Level 1 - Username and Password Only

---
+++ app.js
@@ -0,0 +1,84 @@
+//jshint esversion:6
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, function(err) {
+  if (!err) {
+    console.log("Server is connected to 'userDB' Database on port 27017");
+  } else {
+    console.log(err);
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.route("/register")
+
+.get(function(req, res) {
+  res.render("register");
+})
+
+.post(function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.route("/login")
+
+.get(function(req, res) {
+  res.render("login");
+})
+
+.post(function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+
+app.listen(3000, function() {
+  console.log("Server is up and running on port 3000.")
+});
